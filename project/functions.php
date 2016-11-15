d14f23bb81ea08a1d6071ef71b0766b26df352d0
Jeremiah Smith <jsmit028@seattlecentral.edu>
2016-11-14 19:07:15 -0800

    Add files via upload

--- project/functions.php
+++ project/functions.php
@@ -1,1 +1,21 @@
 <?php register_nav_menus();
+
+
+
+
+    add_action('widgets_init', 'my_sidebar');
+
+    function my_sidebar(){
+        /*Register the primary sidebar*/
+        register_sidebar(
+            array(
+                'id' => 'primary',
+                'names' => ('Primary'),
+                'description' => ('This is the primary sidebar'),
+                'before-widget' => '<div id="%1$s" class="widget%2$s">',
+                'after-widget' => '</div>',
+                'before_title' => '<h3 class="widget-title">',
+                'after_title'=> '</h3>'
+            )
+        );
+    }
